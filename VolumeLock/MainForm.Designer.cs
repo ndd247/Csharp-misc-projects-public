4ef640b654c7ba80ec279f8835293265bd7e89b5
ndd247 <needled247.h@gmail.com>
2021-05-27 14:25:01 +0900

    [MISC] 사소한 U/I 수정.

--- VolumeLock/MainForm.Designer.cs
+++ VolumeLock/MainForm.Designer.cs
@@ -109,7 +109,7 @@ namespace VolumeLock
             this.Controls.Add(this.moTargetVolumeLbl);
             this.Controls.Add(this.moDeviceNameLbl);
             this.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "MainForm";
