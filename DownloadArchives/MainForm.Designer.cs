73032206940b1595badaefd5e6d71602d18f8a03
ndd247 <needled247.h@gmail.com>
2023-01-21 10:02:08 +0900

    [DA][REPO] added initial files for DownloadArchives.

---
+++ DownloadArchives/MainForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace DownloadArchives
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mroCodeInputCBX = new System.Windows.Forms.ComboBox();
+            this.mroNumberSetInputTBX = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.mroFunctionSelectCBX = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mroFastLookupOutputLBL = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mroResultOutputTBX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.mroCurrArchivesOutputTBX = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mroCodeInputCBX
+            // 
+            this.mroCodeInputCBX.Location = new System.Drawing.Point(47, 48);
+            this.mroCodeInputCBX.Name = "mroCodeInputCBX";
+            this.mroCodeInputCBX.Size = new System.Drawing.Size(121, 23);
+            this.mroCodeInputCBX.TabIndex = 4;
+            // 
+            // mroNumberSetInputTBX
+            // 
+            this.mroNumberSetInputTBX.Enabled = false;
+            this.mroNumberSetInputTBX.Location = new System.Drawing.Point(218, 48);
+            this.mroNumberSetInputTBX.Name = "mroNumberSetInputTBX";
+            this.mroNumberSetInputTBX.Size = new System.Drawing.Size(236, 23);
+            this.mroNumberSetInputTBX.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.mroFunctionSelectCBX);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.mroFastLookupOutputLBL);
+            this.groupBox1.Controls.Add(this.mroNumberSetInputTBX);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.mroCodeInputCBX);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.mroResultOutputTBX);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 152);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "컨트롤";
+            // 
+            // mroFunctionSelectCBX
+            // 
+            this.mroFunctionSelectCBX.Appearance = System.Windows.Forms.Appearance.Button;
+            this.mroFunctionSelectCBX.Checked = true;
+            this.mroFunctionSelectCBX.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mroFunctionSelectCBX.Location = new System.Drawing.Point(47, 19);
+            this.mroFunctionSelectCBX.Name = "mroFunctionSelectCBX";
+            this.mroFunctionSelectCBX.Size = new System.Drawing.Size(121, 23);
+            this.mroFunctionSelectCBX.TabIndex = 1;
+            this.mroFunctionSelectCBX.Text = "추가";
+            this.mroFunctionSelectCBX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mroFunctionSelectCBX.UseVisualStyleBackColor = true;
+            this.mroFunctionSelectCBX.CheckedChanged += new System.EventHandler(this.OnCheckedChanged_FunctionSelectCBX);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "기능 :";
+            // 
+            // mroFastLookupOutputLBL
+            // 
+            this.mroFastLookupOutputLBL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mroFastLookupOutputLBL.Location = new System.Drawing.Point(414, 19);
+            this.mroFastLookupOutputLBL.Name = "mroFastLookupOutputLBL";
+            this.mroFastLookupOutputLBL.Size = new System.Drawing.Size(40, 23);
+            this.mroFastLookupOutputLBL.TabIndex = 2;
+            this.mroFastLookupOutputLBL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "번호 :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "코드 :";
+            // 
+            // mroResultOutputTBX
+            // 
+            this.mroResultOutputTBX.Location = new System.Drawing.Point(47, 77);
+            this.mroResultOutputTBX.Multiline = true;
+            this.mroResultOutputTBX.Name = "mroResultOutputTBX";
+            this.mroResultOutputTBX.ReadOnly = true;
+            this.mroResultOutputTBX.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.mroResultOutputTBX.Size = new System.Drawing.Size(407, 69);
+            this.mroResultOutputTBX.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "결과 :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.mroCurrArchivesOutputTBX);
+            this.groupBox2.Location = new System.Drawing.Point(12, 170);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(460, 379);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "현재 데이터";
+            // 
+            // mroCurrArchivesOutputTBX
+            // 
+            this.mroCurrArchivesOutputTBX.Location = new System.Drawing.Point(6, 22);
+            this.mroCurrArchivesOutputTBX.Multiline = true;
+            this.mroCurrArchivesOutputTBX.Name = "mroCurrArchivesOutputTBX";
+            this.mroCurrArchivesOutputTBX.ReadOnly = true;
+            this.mroCurrArchivesOutputTBX.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.mroCurrArchivesOutputTBX.Size = new System.Drawing.Size(445, 351);
+            this.mroCurrArchivesOutputTBX.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(342, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "빠른 룩업 :";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "DownloadArchives";
+            this.Load += new System.EventHandler(this.OnLoad_MainForm);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox mroCodeInputCBX;
+        private System.Windows.Forms.TextBox mroNumberSetInputTBX;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mroCurrArchivesOutputTBX;
+        private System.Windows.Forms.Label mroFastLookupOutputLBL;
+        private System.Windows.Forms.TextBox mroResultOutputTBX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox mroFunctionSelectCBX;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ DownloadArchives/MainForm.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadArchives
+{
+    public partial class MainForm : Form
+    {
+        private static string DEF_CONFIG_PATH = "./DownloadArchives.config.txt";
+
+        private string msArchivesPath = null;
+
+        private bool misArchivesChanged = false;
+        private Dictionary<string, SortedSet<uint>> mroCurrArchives = null;
+
+        private bool misErrorOccured = false;
+
+        private void ReportResult(string asMsg)
+        {
+            mroResultOutputTBX.Text = asMsg;
+        }
+
+        private void PopUpFatalError(string asMsg)
+        {
+            misErrorOccured = true;
+            mroFunctionSelectCBX.Enabled = false;
+            mroCodeInputCBX.Enabled = false;
+            mroNumberSetInputTBX.Enabled = false;
+
+            MessageBox.Show("E: FATAL: " + asMsg);
+        }
+
+        private string AcquireArchivesPathFromConfigFile()
+        {
+            string sArchivesPathRet = null;
+
+            StreamReader rSR = null;
+            try
+            {
+                try { rSR = File.OpenText(DEF_CONFIG_PATH); }
+                catch { throw new Exception("CONFIG 파일(" + DEF_CONFIG_PATH + ")을 열지 못했습니다.");  }
+
+                string sLastTrimmedReadLine = null;
+                while (rSR.Peek() >= 0)
+                {
+                    sLastTrimmedReadLine = rSR.ReadLine().Trim();
+                    if ("" != sLastTrimmedReadLine)
+                        break;
+                }
+
+                if (null == sLastTrimmedReadLine || "" == sLastTrimmedReadLine)
+                    throw new Exception("CONFIG 파일(" + DEF_CONFIG_PATH + ")에는 유의미한 내용이 없습니다.");
+
+                sArchivesPathRet = sLastTrimmedReadLine;
+            }
+            catch (Exception roE) { PopUpFatalError(roE.Message); }
+            finally { rSR?.Close(); rSR = null; }
+
+            return sArchivesPathRet;
+        }
+
+        private Dictionary<string, SortedSet<uint>> AcquireArchivesFromFile(string asArchivesPath)
+        {
+            Dictionary<string, SortedSet<uint>> roArchivesRet = new Dictionary<string, SortedSet<uint>>();
+
+            StreamReader rSR = null;
+            try
+            {
+                try { rSR = File.OpenText(asArchivesPath); }
+                catch { throw new Exception("ARCHIVES 파일(" + asArchivesPath + ")을 열지 못했습니다."); }
+
+                SortedSet<uint> roCurrNumberSet = null;
+                while (rSR.Peek() >= 0)
+                {
+                    string sTrimmedLine = rSR.ReadLine().Trim();
+                    if ("" == sTrimmedLine)
+                        continue;
+
+                    if (sTrimmedLine.StartsWith("@"))
+                    {
+                        string sCode = sTrimmedLine.Substring(1);
+                        if ("" != sCode)
+                        {
+                            if (roArchivesRet.Keys.Contains(sCode))
+                                throw new Exception("코드 섹션이 중복(" + sCode + ")됩니다.");
+
+                            roCurrNumberSet = roArchivesRet[sCode] = new SortedSet<uint>();
+                        }
+                        else
+                            throw new Exception("코드 섹션의 이름이 없습니다.");
+                    }
+                    else
+                    {
+                        string[] sNumberTerms = sTrimmedLine.Split(new char[] { ' ', '\t' });
+                        foreach (string sCurrNumber in sNumberTerms)
+                        {
+                            uint uiNumber = 0;
+                            try { uiNumber = uint.Parse(sCurrNumber); }
+                            catch { throw new Exception("번호로 변환이 안되는 번호 데이터(" + sCurrNumber + ")가 있습니다."); }
+
+                            if (null == roCurrNumberSet)
+                                throw new Exception("코드 섹션 없이 번호 데이터가 먼저 나왔습니다.");
+
+                            roCurrNumberSet.Add(uiNumber);
+                        }
+                    }
+                }
+            }
+            catch (Exception roE) { PopUpFatalError(roE.Message); }
+            finally { rSR?.Close(); rSR = null; }
+
+            return roArchivesRet;
+        }
+
+        private string AcquireCodeFromUI()
+        {
+            return mroCodeInputCBX.Text.Trim();
+        }
+
+        private SortedSet<uint> AcquireNumberSetFromUI()
+        {
+            SortedSet<uint> roNumberSet = new SortedSet<uint>();
+
+            string sTrimmedRawNumbers = mroNumberSetInputTBX.Text.Trim();
+            string[] sNumberTerms = sTrimmedRawNumbers.Split(new char[] { ' ', '\t', ',' });
+
+            foreach (string sCurrNumberTerm in sNumberTerms)
+            {
+                try { roNumberSet.Add(uint.Parse(sCurrNumberTerm)); }
+                catch { /*IGNORE*/ }
+            }
+
+            return roNumberSet;
+        }
+
+        private void ShowCodesToUI()
+        {
+            mroCodeInputCBX.Items.Clear();
+            foreach (string sCurrCode in mroCurrArchives.Keys)
+                mroCodeInputCBX.Items.Add(sCurrCode);
+        }
+
+        private void ShowCurrArchivesToUI()
+        {
+            StringBuilder roSB = new StringBuilder();
+            foreach (KeyValuePair<string, SortedSet<uint>> roCurrPair in mroCurrArchives)
+            {
+                roSB.AppendLine("@" + roCurrPair.Key);
+
+                bool isFirstTime = true;
+                foreach (uint uiCurrNumber in roCurrPair.Value)
+                {
+                    if (isFirstTime)
+                        isFirstTime = false;
+                    else
+                        roSB.Append(" ");
+
+                    roSB.Append(uiCurrNumber);
+                }
+                roSB.AppendLine();
+                roSB.AppendLine();
+            }
+
+            mroCurrArchivesOutputTBX.Text = roSB.ToString();
+        }
+
+        private void FlushArchivesToFile(string asArchivesPath)
+        {
+            StreamWriter roSW = null;
+            try
+            {
+                try { roSW = new StreamWriter(asArchivesPath); }
+                catch { throw new Exception("FATAL: ARCHIVES 파일(\" + asArchivesPath + \")을 열지 못했습니다.");  }
+
+                try
+                {
+                    foreach (KeyValuePair<string, SortedSet<uint>> roCurrPair in mroCurrArchives)
+                    {
+                        roSW.WriteLine("@" + roCurrPair.Key);
+
+                        SortedSet<uint> roNumberSet = roCurrPair.Value;
+
+                        bool isFirstTime = true;
+                        foreach (uint uiNumber in roNumberSet)
+                        {
+                            if (isFirstTime)
+                                isFirstTime = false;
+                            else       
+                                roSW.Write(" ");
+
+                            roSW.Write(uiNumber);
+                        }
+
+                        roSW.WriteLine();
+                        roSW.WriteLine();
+                    }
+                }
+                catch { throw new Exception("FATAL: ARCHIVES 파일(\" + asArchivesPath + \")을 쓰는 도중에 예상치 못한 오류가 발생했습니다.");  }
+            }
+            catch (Exception roE) { PopUpFatalError(roE.Message); }
+            finally { roSW?.Close(); roSW = null; }
+        }
+
+        private void AddToArchives(string asCode, SortedSet<uint> aroNumberSet)
+        {
+            bool isNeedToUpdateCodesToUI = false;
+            bool isArchivesChanged = false;
+
+            StringBuilder roSB = new StringBuilder();
+
+            if (!mroCurrArchives.Keys.Contains(asCode))
+            {
+                mroCurrArchives[asCode] = aroNumberSet;
+                isNeedToUpdateCodesToUI = true;
+                isArchivesChanged = true;
+
+                roSB.AppendLine("I: 코드 섹션 \'" + asCode + "\' 을/를 새로 만들었습니다.");
+                roSB.Append("I: 코드 섹션 \'" + asCode + "\' 에서 ");
+
+                bool isFirstTime = true;
+                foreach (uint uiNumber in aroNumberSet)
+                {
+                    if (isFirstTime)
+                        isFirstTime = false;
+                    else
+                        roSB.Append(", ");
+
+                    roSB.Append(uiNumber);
+                }
+
+                roSB.AppendLine(" 을/를 추가했습니다.");
+            }
+            else
+            {
+                SortedSet<uint> roCurrNumberSet = mroCurrArchives[asCode];
+
+                roSB.Append("I: 코드 섹션 " + asCode + " 에서 ");
+
+                bool isFirstTime = true;
+                foreach (uint uiNumber in aroNumberSet)
+                {
+                    if (isFirstTime)
+                        isFirstTime = false;
+                    else
+                        roSB.Append(", ");
+
+                    if (!roCurrNumberSet.Contains(uiNumber))
+                    {
+                        roCurrNumberSet.Add(uiNumber);
+                        isArchivesChanged = true;
+                        roSB.Append(uiNumber);
+                    }
+                    else
+                        roSB.Append("(" + uiNumber + ")");
+                }
+
+                roSB.AppendLine(" 을/를 추가했습니다.");
+            }
+
+            ReportResult(roSB.ToString());
+
+            if (isNeedToUpdateCodesToUI)
+                ShowCodesToUI();
+
+            if (isArchivesChanged)
+            {
+                misArchivesChanged = true;
+                ShowCurrArchivesToUI();
+            }
+        }
+
+        private void RemoveFromArchives(string asCode, SortedSet<uint> aroNumberSet)
+        {
+            bool isNeedToUpdateCodesToUI = false;
+            bool isArchivesChanged = false;
+
+            StringBuilder roSB = new StringBuilder();
+
+            if (mroCurrArchives.Keys.Contains(asCode))
+            {
+                SortedSet<uint> roCurrNumberSet = mroCurrArchives[asCode];
+
+                roSB.Append("I: 코드 섹션 \'" + asCode + "\' 에서 ");
+
+                bool isFirstTime = true;
+                foreach (uint uiNumber in aroNumberSet)
+                {
+                    if (isFirstTime)
+                        isFirstTime = false;
+                    else
+                        roSB.Append(", ");
+
+                    if (roCurrNumberSet.Contains(uiNumber))
+                    {
+                        roCurrNumberSet.Remove(uiNumber);
+                        isArchivesChanged = true;
+                        roSB.Append(uiNumber);
+                    }
+                    else
+                        roSB.Append("(" + uiNumber + ")");
+                }
+
+                roSB.AppendLine(" 을/를 제거했습니다.");
+
+                if (0 == roCurrNumberSet.Count)
+                {
+                    mroCurrArchives.Remove(asCode);
+                    isNeedToUpdateCodesToUI = true;
+
+                    roSB.AppendLine("I: 코드 섹션 \'" + asCode + "\' 을/를 제거했습니다.");
+                }
+            }
+            else
+                roSB.AppendLine("W: 코드 섹션 \'" + asCode +"\' 은/는 존재하지 않습니다. 따라서 아무런 처리도 이루어지지 않았습니다.");
+
+            ReportResult(roSB.ToString());
+
+            if (isNeedToUpdateCodesToUI)
+                ShowCodesToUI();
+
+            if (isArchivesChanged)
+            {
+                misArchivesChanged = true;
+                ShowCurrArchivesToUI();
+            }
+        }
+
+        private void ProcessFastLookup(string asCode, uint auiNumber)
+        {
+            if (mroCurrArchives.Keys.Contains(asCode))
+            {
+                if (mroCurrArchives[asCode].Contains(auiNumber))
+                    mroFastLookupOutputLBL.Text = "O";
+                else
+                    mroFastLookupOutputLBL.Text = "X";
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeComponentMy();
+        }
+
+        void InitializeComponentMy()
+        {
+            this.FormClosing += OnFormClosing_MainForm;
+            mroCodeInputCBX.TextChanged += OnTextChanged_CodeInputCBX;
+            mroNumberSetInputTBX.TextChanged += OnTextChanged_NumberSetInputTBX;
+            mroNumberSetInputTBX.KeyUp += OnKeyUp_NumberSetInputTBX;
+        }
+
+        private void OnLoad_MainForm(object aroS, EventArgs aroE)
+        {
+            if (null != (msArchivesPath = AcquireArchivesPathFromConfigFile()))
+            {
+                if (null != (mroCurrArchives = AcquireArchivesFromFile(msArchivesPath)))
+                {
+                    ShowCodesToUI();
+                    ShowCurrArchivesToUI();
+                }
+            }
+        }
+
+        private void OnFormClosing_MainForm(object aroS, FormClosingEventArgs aroE)
+        {
+            if (!misErrorOccured)
+            {
+                if (misArchivesChanged)
+                    FlushArchivesToFile(msArchivesPath);
+            }
+        }
+
+        private void OnCheckedChanged_FunctionSelectCBX(object aroS, EventArgs aroE)
+        {
+            if (mroFunctionSelectCBX.Checked)
+                mroFunctionSelectCBX.Text = "추가";
+            else
+                mroFunctionSelectCBX.Text = "제거";
+        }
+
+        private void OnTextChanged_CodeInputCBX(object aroS, EventArgs aroE)
+        {
+            string sCode = AcquireCodeFromUI();
+            mroNumberSetInputTBX.Enabled = (sCode != "");
+        }
+
+        private void OnTextChanged_NumberSetInputTBX(object aroS, EventArgs aroE)
+        {
+            string sCode = AcquireCodeFromUI();
+            SortedSet<uint> roNumbers = AcquireNumberSetFromUI();
+            if (1 == roNumbers.Count)
+            {
+                ProcessFastLookup(sCode, roNumbers.ElementAt(0));
+                return;
+            }
+
+            mroFastLookupOutputLBL.Text = "";
+        }
+
+        private void OnKeyUp_NumberSetInputTBX(object aroS, KeyEventArgs aroE)
+        {
+            if (aroE.KeyCode == Keys.Enter)
+            {
+                string sCode = AcquireCodeFromUI();
+                SortedSet<uint> roNumbers = AcquireNumberSetFromUI();
+
+                if (roNumbers.Count > 0)
+                {
+                    if (mroFunctionSelectCBX.Checked)
+                        AddToArchives(sCode, roNumbers);
+                    else
+                        RemoveFromArchives(sCode, roNumbers);
+
+                    mroNumberSetInputTBX.Text = "";
+                }
+            }
+        }
+    }
+}
